49177d679e774c1d25408ec71ee44a628b71b9a8
Ravan Naidoo <rnaidoo@suse.com>
2026-04-04 13:28:59 +0200

    feat(csaf): add product tree builder from SBOM components

---
+++ pkg/csaf/product_tree.go
@@ -0,0 +1,31 @@
+package csaf
+
+import "github.com/ravan/suse-cra-toolkit/pkg/formats"
+
+func buildProductTree(components []formats.Component, publisherName string) productTree {
+	productBranches := make([]branch, 0, len(components))
+	for _, c := range components {
+		helper := &piHelper{PURL: c.PURL}
+		for algo, val := range c.Hashes {
+			helper.Hashes = append(helper.Hashes, piHash{Algorithm: algo, Value: val})
+		}
+		versionBranch := branch{
+			Category: "product_version",
+			Name:     c.Version,
+			Product: &product{
+				Name:      c.Name + "@" + c.Version,
+				ProductID: c.PURL,
+				PIHelper:  helper,
+			},
+		}
+		productBranch := branch{
+			Category: "product_name",
+			Name:     c.Name,
+			Branches: []branch{versionBranch},
+		}
+		productBranches = append(productBranches, productBranch)
+	}
+	return productTree{
+		Branches: []branch{{Category: "vendor", Name: publisherName, Branches: productBranches}},
+	}
+}
